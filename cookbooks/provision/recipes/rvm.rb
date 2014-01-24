9d9e9b0eb1b42ae91ce475a2a0888664c90eea86
JJ Asghar <jjasghar@gmail.com>
2014-01-24 17:12:30 -0600

    rvm updates

--- cookbooks/provision/recipes/rvm.rb
+++ cookbooks/provision/recipes/rvm.rb
@@ -11,8 +11,9 @@ bash "install and setup rvm" do
   creates "/Users/#{node['current_user']}/.rvm/"
   code <<-EOH
   STATUS=0
-  curl -sSL https://get.rvm.io | bash -s stable" || STATUS=1
-  source ~/.bash_profile" || STATUS=1
+  curl -sSL https://get.rvm.io | bash -s stable -- --ignore-dotfiles" || STATUS=1
+  echo "source $HOME/.rvm/scripts/rvm" >> $HOME/.bash_profile
+  source $HOME/.bash_profile" || STATUS=1
   rvm install 1.9.2 || STATUS=1
   rvm install 2.0.0 || STATUS=1
   rvm install 2.1.0 || STATUS=1
