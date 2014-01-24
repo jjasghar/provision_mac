7b5a5d3f53765699b674fcb3f885186da32df2cf
JJ Asghar <jjasghar@gmail.com>
2014-01-24 16:30:48 -0600

    updated rvm to be more better

--- cookbooks/provision/recipes/rvm.rb
+++ cookbooks/provision/recipes/rvm.rb
@@ -5,4 +5,20 @@
 #
 
 
-execute "curl -sSL https://get.rvm.io | bash -s stable"
+
+bash "install and setup rvm" do
+  cwd "~"
+  creates "~/.rvm"
+  code <<-EOH
+  STATUS=0
+  echo progress-bar >> ~/.curlrc" || STATUS=1
+  curl -sSL https://get.rvm.io | bash -s stable" || STATUS=1
+  source ~/.bash_profile" || STATUS=1
+  rvm install 1.9.2 || STATUS=1
+  rvm install 2.0.0 || STATUS=1
+  rvm install 2.1.0 || STATUS=1
+  rvm --default 2.0.0 || STATUS=1
+  exit $STATUS
+  EOH
+end
+
