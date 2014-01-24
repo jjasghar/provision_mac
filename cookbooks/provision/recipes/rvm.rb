e517dc6fc15ae735f187762ee58fce01a7764d9b
JJ Asghar <jjasghar@gmail.com>
2014-01-24 17:35:01 -0600

    more rvm

--- cookbooks/provision/recipes/rvm.rb
+++ cookbooks/provision/recipes/rvm.rb
@@ -4,20 +4,24 @@
 #
 #
 
-
-
-bash "install and setup rvm" do
+execute "install rvm" do
+  command "curl -sSL https://get.rvm.io | bash -s stable"
   cwd "/Users/#{node['current_user']}"
   creates "/Users/#{node['current_user']}/.rvm/"
+end
+
+bash "setup rvm" do
+  cwd "/Users/#{node['current_user']}"
+  creates "/Users/#{node['current_user']}/.rvm/im_installed_bitch"
   code <<-EOH
   STATUS=0
-  curl -sSL https://get.rvm.io | bash -s stable || STATUS=1
   echo "source $HOME/.rvm/scripts/rvm" >> $HOME/.bash_profile
   source $HOME/.bash_profile" || STATUS=1
   rvm install 1.9.2 || STATUS=1
   rvm install 2.0.0 || STATUS=1
   rvm install 2.1.0 || STATUS=1
   rvm --default 2.0.0 || STATUS=1
+  touch /Users/#{node['current_user']}/.rvm/im_installed_bitch
   exit $STATUS
   EOH
 end
