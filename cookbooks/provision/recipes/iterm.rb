8a1433b1d4b79006727bd5627c13b493d48700e3
JJ Asghar <jjasghar@gmail.com>
2014-01-25 11:36:56 -0600

    updated chorme and iterm

--- cookbooks/provision/recipes/iterm.rb
+++ cookbooks/provision/recipes/iterm.rb
@@ -4,16 +4,23 @@
 #
 #
 
-remote_file "/tmp/remote_file" do
-  owner "root"
-  group "root"
+remote_file "/Users/#{node['current_user']}/Downloads/iterm2.zip" do
+  owner "#{node['current_user']}"
+  group "#{node['current_user']}"
   mode "0644"
-  source "http://www.example.com/remote_file"
-  checksum "sha256checksum"
+  source "http://www.iterm2.com/downloads/stable/iTerm2_v1_0_0.zip"
 end
 
-#unzip
-
-#move to /Applications
+bash "unzip and copy iTerm2" do
+  user "#{node['current_user']}"
+  cwd "/Users/#{node['current_user']}/Downloads/"
+  creates "/Applications/iTerm.app"
+  code <<-EOH
+  STATUS=0
+  unzip iTerm2_v1_0_0.zip || STATUS=1
+  mv iTerm.app /Applications/ || STATUS=1
+  exit $STATUS
+  EOH
+end
 
 
