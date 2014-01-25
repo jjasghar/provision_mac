14ccb15e17117b56167cc568a1efd36df3649010
JJ Asghar <jjasghar@gmail.com>
2014-01-24 18:31:42 -0600

    updated with the reop

--- cookbooks/provision/recipes/default.rb
+++ cookbooks/provision/recipes/default.rb
@@ -10,5 +10,6 @@ include_recipe 'provision::base_pkg'
 include_recipe 'provision::mysql'
 include_recipe 'provision::redis'
 include_recipe 'provision::rvm'
+include_recipe 'provision::repo'
 
 
---
+++ cookbooks/provision/recipes/repo.rb
@@ -0,0 +1,33 @@
+#
+#
+# main provision recipe
+#
+#
+
+
+
+directory "/Users/#{node['current_user']}/repo/" do
+  owner "#{node['current_user']}"
+  group "staff"
+  action :create
+end
+
+git "/Users/#{node['current_user']}/repo/" do
+  repository "git@github.com:PeopleAdmin/portals.git"
+  reference "master"
+  user "#{node['current_user']}"
+  group "staff"
+  action :sync
+end
+
+bash "run bundle" do
+  user "#{node['current_user']}"
+  cwd "/Users/#{node['current_user']}/repo/"
+  code <<-EOH
+  STATUS=0
+  gem install bundler || STATUS=1
+  bundle || STATUS=1
+  exit $STATUS
+  EOH
+end
+
