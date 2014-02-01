b7b465355f6a5b786a516b843b5fae75027c7777
JJ Asghar <jjasghar@gmail.com>
2014-02-01 13:58:51 -0600

    grrr

--- cookbooks/provision/recipes/mysql.rb
+++ cookbooks/provision/recipes/mysql.rb
@@ -6,4 +6,17 @@
 
 execute "brew install homebrew/versions/mysql55 --llvm-gcc"
 
+bash "set up mysql55 lauch agent" do
+  user "#{node['current_user']}"
+  cwd "/User/#{node['current_user']}"
+  creates "/User/#{node['current_user']}/Library/LaunchAgents/homebrew.mxcl.mysql55.plist"
+  code <<-EOH
+  STATUS=0
+  mkdir -p ~/Library/LaunchAgents/   || STATUS=1
+  cp /usr/local/Cellar/mysql55/5.5.30/homebrew.mxcl.mysql55.plist ~/Library/LaunchAgents   || STATUS=1
+  launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.mysql55.plist   || STATUS=1
+  exit $STATUS
+  EOH
+end
+
 execute "brew services restart mysql55"
